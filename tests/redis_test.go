8dafafbc59fc186820d6bef68835bbf2ee94b60d
Sudipto <sudipto00003@gmail.com>
2022-01-13 02:03:16 +0600

    redis service test passed

---
+++ tests/redis_test.go
@@ -0,0 +1,35 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+func TestRedisConnection(t *testing.T) {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "0.0.0.0:6379",
+		DB:   0,
+	})
+	val := "hello"
+	err := rdb.Set(ctx, "key", val, 0).Err()
+	if err != nil {
+		t.Fatalf("%q\n", err)
+	}
+
+	value, err := rdb.Get(ctx, "key").Result()
+	if err == redis.Nil {
+		t.Errorf("key don't exist in redis")
+	}
+	if err != nil {
+		t.Fatalf("%q\n", err)
+	}
+
+	if val != value {
+		t.Errorf("Value mismatch. want=%q got=%q", val, value)
+	}
+}
