ba95485ea0b346579a0f841a8a519aa3cb5d946e
Sudipto <sudipto00003@gmail.com>
2022-01-29 02:49:04 +0600

    dates are parsed as primitive DateTime

--- models/parcel.go
+++ models/parcel.go
@@ -1,13 +1,39 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"encoding/json"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type pTime primitive.DateTime
+
+const timeFormat = "02-01-2006"
+
+func (pt *pTime) UnmarshalJSON(bs []byte) error {
+	var timestr string
+	err := json.Unmarshal(bs, &timestr)
+	if err != nil {
+		return err
+	}
+
+	ptime, err := time.Parse(timeFormat, timestr)
+	*pt = pTime(primitive.NewDateTimeFromTime(ptime))
+	return err
+}
+
+func (pt pTime) MarshalJSON() ([]byte, error) {
+	timestr := primitive.DateTime(pt).Time().Format(timeFormat)
+	return json.Marshal(timestr)
+}
 
 type Parcel struct {
 	ID         primitive.ObjectID `json:"_id" bson:"_id" binding:"-"`
 	ParcelId   string             `json:"parcel_id"`
 	Weight     float32            `json:"weight"`
 	Status     string             `json:"status"`
-	PickedDate string             `json:"date_picked"`
+	PickedDate pTime              `json:"date_picked" bson:"date_picked"`
 	Sender     AddressDetail      `json:"sender"`
 	Receiver   AddressDetail      `json:"receiver"`
 }
